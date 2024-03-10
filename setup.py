dc56dc4c576d87126d1f54b4a5ed3e14980b5118
Sokinjo <160136911+SokinjoNS@users.noreply.github.com>
2024-03-10 13:51:56 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="data-exporter",
-    version="0.1.0",
+    version="0.1.1",
     author="SokinjoNS",
     author_email="sokinjo.155@gmail.com",
     description="A module for exporting email data to CSV format.",
@@ -10,7 +10,7 @@ setup(
     url="https://github.com/SokinjoNS/data-exporter",
     packages=find_packages(),
     install_requires=[
-        'gmail-message-processor>=0.1.0',
+        'gmail-message-processor>=0.1.1',
     ],
     classifiers=[
         'Development Status :: 3 - Alpha',
